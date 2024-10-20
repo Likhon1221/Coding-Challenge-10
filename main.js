834751bc0fae7555f97e4f0b3883d60e026e784e
Likhon1221 <siddiqur.rahman1221@gmail.com>
2024-10-20 17:13:18 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -8,3 +8,28 @@ sizeSelector.addEventListener('change', function(event) {
     priceElement.textContent = `$${selectedPrice}`;
     }
 );
+
+// Task 3. Handle Stock Availability
+
+let stockStatusElement = document.getElementById('stock-status');
+let purchaseButton = document.getElementById('purchase-button');
+
+const stock = {
+    small: { price: 20, available: 15 },  
+    medium: { price: 25, available: 17 }, 
+    large: { price: 30, available: 0 }  
+}
+sizeSelector.addEventListener('change', function(event) {
+    let selectedSize = event.target.value;  
+    let selectedStock = stock[selectedSize];  
+    priceElement.textContent = `$${selectedStock.price}`;
+
+    if (selectedStock.available) {
+        stockStatusElement.textContent = "In Stock";
+        purchaseButton.disabled = false;  
+    } else {
+        stockStatusElement.textContent = "Out of Stock";
+        purchaseButton.disabled = true;  
+    }
+    }
+);
